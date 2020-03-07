365bed9783a8f943b47eb03264932732f1305348
IzzelAliz <csh2001331@126.com>
2020-03-08 02:22:32 +0800

    不知道什么毛病，反正就这样了

--- src/main/scala/io/izzel/taboolib/loader/ScalaPlugin.scala
+++ src/main/scala/io/izzel/taboolib/loader/ScalaPlugin.scala
@@ -1,20 +1,12 @@
 package io.izzel.taboolib.loader
 
 import org.bukkit.plugin.Plugin
-import org.yaml.snakeyaml.Yaml
-
-import scala.jdk.CollectionConverters._
-import java.util.{Map => JMap}
-
 import org.bukkit.plugin.java.JavaPlugin
 
 class ScalaPlugin extends ScalaPluginInterface {
 
   private val plugin = {
-    val stream = getClass.getResourceAsStream("/plugin.yml")
-    val map = new Yaml().load(stream).asInstanceOf[JMap[String, Any]].asScala
-    val cl = Class.forName(map("main").toString).asSubclass(classOf[JavaPlugin])
-    JavaPlugin.getPlugin(cl)
+    JavaPlugin.getPlugin(classOf[ScalaLoader])
   }
 
   override def onLoading(): Unit = {}
