15f42fb2bc83319ce92bbbdb2d5f6d0543e156c9
IzzelAliz <csh2001331@126.com>
2020-03-08 01:57:49 +0800

    更新仓库地址

--- src/main/scala/io/izzel/taboolib/loader/ScalaLoader.java
+++ src/main/scala/io/izzel/taboolib/loader/ScalaLoader.java
@@ -1,5 +1,6 @@
 package io.izzel.taboolib.loader;
 
+import io.izzel.taboolib.module.dependency.Dependency;
 import io.izzel.taboolib.module.dependency.TDependency;
 import org.yaml.snakeyaml.Yaml;
 
@@ -7,13 +8,13 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.Map;
 
+@Dependency(maven = "org.scala-lang:scala-library:2.13.1", mavenRepo = "https://maven.aliyun.com/repository/central")
 public class ScalaLoader extends BasePlugin {
 
     private ScalaPluginInterface scalaMain;
 
     @Override
     public void onLoading() {
-        TDependency.requestLib("org.scala-lang:scala-library:2.13.1",TDependency.MAVEN_REPO,"");
         InputStream stream = getClass().getResourceAsStream("/plugin.yml");
         Object load = new Yaml().load(stream);
         String main = (String) ((Map<?, ?>) load).get("scalaMain");
